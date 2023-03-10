f2bb4439834d4dcb6935e8107563b34568bffb55
zuriBarnes <zuri@dentalrevenue.com>
2023-03-09 19:23:43 -0500

    connected urls, and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    name = "Zuri"
+    return render(request, 'blog/post_list.html', {"name": name})
