f3b1d13a911cb9cf9f428889ff57919efdcb7e3b
zuriBarnes <zuri@dentalrevenue.com>
2023-03-14 13:21:59 -0400

    edited models, added/edited templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    name = "Zuri"
-    return render(request, 'blog/post_list.html', {"name": name})
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {"posts": posts})
