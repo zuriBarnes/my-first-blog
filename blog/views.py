2235ebf8f8acdf7ea7f8e0131b8021c06bb4a40e
zuriBarnes <zuri@dentalrevenue.com>
2023-03-14 14:19:12 -0400

    more model edits

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all().order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('title')
     return render(request, 'blog/post_list.html', {"posts": posts})
